7696bc769babc5232a1d8e2c3f9b4937208900ff
Armaan Arora <armaanarora880@gmail.com>
2026-03-10 00:59:37 -0400

    Created some Controller methods and added pictures

--- src/main/java/org/example/card24_game/HelloController.java
+++ src/main/java/org/example/card24_game/HelloController.java
@@ -2,13 +2,63 @@ package org.example.card24_game;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.util.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ImageView card1, card2, card3, card4;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField expressionField;
+
+    @FXML
+    private TextField solutionField;
+
+
+    private final int[] cardValues = new int[4];
+    private final int[] cardIndices = new int[4];
+
+    Random random = new Random();
+
+    @FXML
+    public void initialize(){
+        generateCards();
     }
+
+    private void generateCards(){
+        for (int i = 0; i < 4; i++) {
+            cardIndices[i] = random.nextInt(52) + 1;
+            cardValues[i] = getCardValue(cardIndices[i]);
+        }
+
+        setImage(card1,cardValues[0]);
+        setImage(card1,cardValues[0]);
+        setImage(card1,cardValues[0]);
+        setImage(card1,cardValues[0]);
+    }
+
+    private void setImage(ImageView imageView, int index){
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/example/card24_game/png/" + index + ".png")));
+        imageView.setImage(image);
+    }
+
+    private int getCardValue(int index){
+        int value = index % 13;
+        if (value == 0){
+            return 13;
+        }
+        return value;
+    }
+
+    @FXML
+    private void handleRefresh(){
+        generateCards();
+        expressionField.clear();
+        solutionField.clear();
+    }
+
+
 }
